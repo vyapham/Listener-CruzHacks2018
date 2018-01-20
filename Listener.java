2ca260c6f3a0de8ab2a85a15e0b15af7d39cdb6b
Vy Pham <vyapham95@gmail.com>
2018-01-20 12:22:17 -0800

    Update Listener.java

--- Listener.java
+++ Listener.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Listener {
-	public void quotesInput(ArrayList<String> quotes, String fileName) {
+	public static void quotesInput(ArrayList<String> quotes, String fileName) {
 		try {
 			File file = new File(fileName);
 			FileReader fileReader = new FileReader(file);
@@ -26,14 +26,26 @@ public class Listener {
 
 	public static void main(String[] args) {
 		ArrayList<String> quotes = new ArrayList<String>();
-		String fileName = "";
+		String fileName = "quotes.txt";
+		boolean start = false;
 		
 		int sampleHour = 8;
 		int sampleMinute = 00;
 		int sampleSecond = 00;
 		
 		PrintQuote p = new PrintQuote(sampleHour, sampleMinute, sampleSecond);
+		quotesInput(quotes, fileName);
 		
-		// ........
+		// ......
+		int cursor = 0;
+		while (start) {
+			if (cursor >= quotes.size()) {
+				cursor = 0;
+			}
+			if (p.equals(p)) {
+				System.out.println(quotes.get(cursor));
+			}
+			// What if users decide to disable this feature
+		}
 	}
 }
